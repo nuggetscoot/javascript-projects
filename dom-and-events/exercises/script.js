db15a53d978896e7a66046a006118306b3663def
Karly Berrning <berningkarly@Karlys-MacBook-Air.local>
2023-10-29 20:34:30 -0500

    finished project

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
-}
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+
+missionAbort.addEventListener("mouseenter", function() {
+  missionAbort.style.backgroundColor = "red";
+});
+missionAbort.addEventListener("mouseleave", function() {
+  missionAbort.style.backgroundColor = "";
+});
+
+     missionAbort.addEventListener("click", function() {
+        const confirmAbort = confirm("Are you sure you want to abort the mission?");
+      
+        if (confirmAbort) {
+          statusReport.textContent = "Mission aborted! Space shuttle returning home.";
+        }
+});
+};
 
 window.addEventListener("load", init);
